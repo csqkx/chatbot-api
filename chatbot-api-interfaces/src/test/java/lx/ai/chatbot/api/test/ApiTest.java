174b7591f8d9e556813dfa674f3b0864dc6b6ce3
NCUT-Kichen <2868414230@qq.com>
2024-07-11 15:05:53 +0800

    feat:写入了爬取问题和回答问题的接口

--- chatbot-api-interfaces/src/test/java/lx/ai/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/lx/ai/chatbot/api/test/ApiTest.java
@@ -1,6 +1,57 @@
 package lx.ai.chatbot.api.test;
 
 
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class ApiTest {
+    @Test
+    public void query_unanswered_question() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet get = new HttpGet("https://api.zsxq.com/v2/groups/28885518425541/topics?scope=all&count=20");
+        get.addHeader("cookie","zsxq_access_token=6B5A36D9-C1DB-48B3-3ABA-6513B53180E3_8B25F07015DC7E0B; abtest_env=product; zsxqsessionid=56c9f421bbba2032f07713d8347495f6\n");
+        get.addHeader("Content-Type","application/json, text/plain, */*");
+        CloseableHttpResponse response = httpClient.execute(get);
+        if(response.getStatusLine().getStatusCode()== HttpStatus.SC_OK){
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        }else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
+    @Test
+    public void answer() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost("https://api.zsxq.com/v2/topics/1522115885851482/comments");
+        post.addHeader("cookie","zsxq_access_token=6B5A36D9-C1DB-48B3-3ABA-6513B53180E3_8B25F07015DC7E0B; abtest_env=product; zsxqsessionid=56c9f421bbba2032f07713d8347495f6\n");
+        post.addHeader("Content-Type","application/json, text/plain, */*");
+        String paramJson="{\n" +
+                "  \"req_data\": {\n" +
+                "    \"text\": \"一会告诉你！！！\\n\",\n" +
+                "    \"image_ids\": [],\n" +
+                "    \"mentioned_user_ids\": []\n" +
+                "  }\n" +
+                "}";
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));
+        post.setEntity(stringEntity);
+        CloseableHttpResponse response = httpClient.execute(post);
+
+        if(response.getStatusLine().getStatusCode()== HttpStatus.SC_OK){
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        }else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
 
 }
